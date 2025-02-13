a81d10bd11ff964b77a8229d602481f7478ba194
siva0199 <v.sivakrishna1999@gmail.com>
2025-02-13 15:33:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Hello, Siva! Your app is looking for updates "
+    return "Hello, Siva! Your app is looking for changes now "
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

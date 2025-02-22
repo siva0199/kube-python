2bc92d48ce86d989b4ea757eb1d92779b0125c3b
siva0199 <v.sivakrishna1999@gmail.com>
2025-02-22 16:50:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Hello, Siva! Your this is argocd changes"
+    return "Hello, Siva! Your argocd image detect find the update version"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

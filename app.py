7de55f15c19a95226322770e4f2336c0690556a8
siva0199 <v.sivakrishna1999@gmail.com>
2025-02-24 12:06:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "Hello, Siva! Your argocd image detect image correctly or not check it once do it again once more"
+    return "Hello, Siva! Your argocd image detect image correctly or not check it once do it again once more and one more thing is now i am checking new version container"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

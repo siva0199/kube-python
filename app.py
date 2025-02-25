de15ad685ff9dc043c34b3eb0466c44db4046a3f
root <root@ip-172-31-49-172.ec2.internal>
2025-02-25 10:53:39 +0000

    the only bharath

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "this hunter dragon will happen next"
+    return "come on kill that bitch motherfuckkker"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

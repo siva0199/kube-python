6573294ba615f829328dd4fadd6e99a5834a0653
root <root@ip-172-31-80-192.ec2.internal>
2025-02-25 13:35:34 +0000

    jneiw onw

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "the one and only man"
+    return "the one house "
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

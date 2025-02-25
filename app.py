220d75987a1042153f5b33a19125452a9d8cf471
root <root@ip-172-31-49-172.ec2.internal>
2025-02-25 10:35:30 +0000

    new version here

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "this one final new version which will happen next"
+    return "this hunter dragon will happen next"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

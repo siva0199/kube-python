3a17a7f6b995720cbb617f27532d5da12e5a9e8f
siva0199 <v.sivakrishna1999@gmail.com>
2025-02-25 11:27:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return "you motherfuckers i finally got it..."
+    return "this one is the new version"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
